84f83a8f2ae2cf8dd2e53bce2b0a12aea6e05503
mitatskni <mitatskni@gmail.com>
2023-04-22 09:37:34 +0200

    Initial commit

--- Frenzo/settings.py
+++ Frenzo/settings.py
@@ -9,24 +9,22 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
-
+from .. import local_settings
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-h0n9hx*dr8(m%f^bmol_wux2o4$jguqktoizp=k2#tacx%yhft'
+SECRET_KEY = local_settings.SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 ALLOWED_HOSTS = []
-
 
 # Application definition
 
@@ -69,17 +67,26 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'Frenzo.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
+
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+
+        'ENGINE': 'django.db.backends.postgresql',
+
+        'NAME': local_settings.name_bd,  # имя бд (у меня по умолнчанию)
+
+        'USER': local_settings.user_bd,  # имя пользователя (у меня по умолнчанию)
+
+        'PASSWORD': local_settings.password_bd,  # пароль при входе в субд
+
+        'HOST': local_settings.host_bd,  # тут по документации django можно оставить пустым или localhost
+
+        'PORT': local_settings.port_bd
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
@@ -99,7 +106,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
 
@@ -110,7 +116,6 @@ TIME_ZONE = 'UTC'
 USE_I18N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
