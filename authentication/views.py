aa9cbafa3e3652cbb29446bf2cd303a43423d7ec
mitatskni <mitatskni@gmail.com>
2023-04-23 19:00:12 +0200

    Add LoginView, RegisterView, and IndexView to authentication app views

--- authentication/views.py
+++ authentication/views.py
@@ -1,3 +1,39 @@
 from django.shortcuts import render
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import login, authenticate
+from django.views.generic.edit import FormView
+from django.views.generic import TemplateView
+from django.urls import reverse_lazy
+from .forms import UserRegistrationForm
+
+
+class LoginView(FormView):
+    template_name = "authentication/login.html"
+    form_class = AuthenticationForm
+    success_url = reverse_lazy('home')
+
+    def form_valid(self, form):
+        username = form.cleaned_data.get('username')
+        password = form.cleaned_data.get('password')
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            login(self.request, user)
+            return super(LoginView, self).form_valid(form)
+        else:
+            return self.form_invalid(form)
+
+
+class RegisterView(FormView):
+    template_name = 'authentication/register.html'
+    form_class = UserRegistrationForm
+    success_url = reverse_lazy('home')
+
+    def form_valid(self, form):
+        form.save()
+        return super(RegisterView, self).form_valid(form)
+
 
 # Create your views here.
+
+class IndexView(TemplateView):
+    template_name = 'authentication/index.html'
