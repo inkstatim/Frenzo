fed10abffa507605b113998c0de0ba9733ee48f6
mitatskni <mitatskni@gmail.com>
2023-04-23 18:56:52 +0200

    Added the group model for working with groups.

--- groups/models.py
+++ groups/models.py
@@ -1,3 +1,11 @@
+from django.conf import settings
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+User = get_user_model()
+
+
+class Group(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    members = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name='user_groups')
