dd487eeefba68315c29a5a23230ef6e3457c48c1
mitatskni <mitatskni@gmail.com>
2023-04-23 18:57:15 +0200

    Added the Notification model for working notifications.

--- notifications/models.py
+++ notifications/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+User = get_user_model()
+
+
+class Notification(models.Model):
+    recipient = models.ForeignKey(User, on_delete=models.CASCADE, related_name='notifications')
+    message = models.TextField()
+    read = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
