dde65efa3714f31a31a61e02e408711a1478b80a
mitatskni <mitatskni@gmail.com>
2023-05-04 12:35:05 +0200

    Added validation to the email field that prevents users from registering with an email address that already exists.

--- account/forms.py
+++ account/forms.py
@@ -22,6 +22,12 @@ class UserRegistrationForm(forms.ModelForm):
             raise forms.ValidationError('Passwords don\'t match.')
         return cd['password2']
 
+    def clean_email(self):
+        data = self.cleaned_data['email']
+        if User.objects.filter(email=data).exists():
+            raise forms.ValidationError('Email already in use.')
+        return data
+
 
 class UserEditForm(forms.ModelForm):
     class Meta:
