1727be8e28f41dced7eaf3c58635fd9c77e1ee26
mitatskni <mitatskni@gmail.com>
2023-05-04 13:17:14 +0200

    We will override the save() method of the Image model to automatically generate the slug field based on the value of the title field. Import the slugify() function and add the save() method to the Image model.

--- images/models.py
+++ images/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.conf import settings
+from django.utils.text import slugify
 
 
 class Image(models.Model):
@@ -15,6 +16,11 @@ class Image(models.Model):
         indexes = [models.Index(fields=['-created'])]
         ordering = ['-created']
 
-        def __str__(self):
-            return self.title
+    def __str__(self):
+        return self.title
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.title)
+        super().save(*args, **kwargs)
 # Create your models here.
