42e043e1d2b088e9a9676d964b9cdc701db8e4be
mitatskni <mitatskni@gmail.com>
2023-05-11 11:45:29 +0200

    They are used to find all DOM elements <img> whose src attribute ends with either .jpg, .jpeg, or .png.

--- images/static/js/bookmarklet.js
+++ images/static/js/bookmarklet.js
@@ -29,6 +29,16 @@ function bookmarkletLaunch() {
         .addEventListener('click', function () {
             bookmarklet.style.display = 'none'
         });
+    // найти изображения в DOM с минимальными размерами
+    images = document.querySelectorAll('img[src$=".jpg"], img[src$=".jpeg"], img[src$="png"]');
+    images.forEach(image => {
+        if (image.naturalWidth >= minWidth
+            && image.naturalHeight >= minHeight) {
+            var imageFound = document.createElement('img');
+            imageFound.src = image.src;
+            imagesFound.append(imageFound);
+        }
+    })
 }
 
 // запустить букмарклет
