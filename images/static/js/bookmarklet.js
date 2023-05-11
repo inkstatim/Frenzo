0fb4eda89ed139677171cf596d16bb394ffcb149
mitatskni <mitatskni@gmail.com>
2023-05-11 11:35:57 +0200

    This source code extracts the <body> element from the DOM model and adds new HTML source code to it by modifying its innerHTML property. A new <div> element is added to the page's body.

--- images/static/js/bookmarklet.js
+++ images/static/js/bookmarklet.js
@@ -8,5 +8,11 @@ var head = document.getElementsByTagName('head')[0];
 var link = document.createElement('link');
 link.rel = 'stylesheet';
 link.type = 'text/css';
-link.href = styleUrl + '?r=' + Math.floor(Math.random()*9999999999999999);
+link.href = styleUrl + '?r=' + Math.floor(Math.random() * 9999999999999999);
 head.appendChild(link);
+
+// загрузить HTML
+
+var body = document.getElementsByTagName('body')[0];
+boxHtml = ' <div id = "bookmarklet"> <a href = "#" id = "close"> & times; </a> <h1>Select an image to bookmark:</h1> <div class="images"></div> </div>';
+body.innerHTML += boxHtml;
