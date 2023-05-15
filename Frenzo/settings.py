66b93547b62fc94bb74c3843300646a9e47fb42f
mitatskni <mitatskni@gmail.com>
2023-05-15 18:57:51 +0200

    Removed the images application.

--- Frenzo/settings.py
+++ Frenzo/settings.py
@@ -36,7 +36,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'images.apps.ImagesConfig',
     'posts.apps.PostsConfig',
     'django_extensions',
 ]
