2517cbc7edcbe70c2e60eca494c09eee4be825a8
mitatskni <mitatskni@gmail.com>
2023-05-15 18:46:38 +0200

    Resolved some issues with comments and implemented a widget for comments.

--- posts/froms.py
+++ posts/froms.py
@@ -12,3 +12,6 @@ class CommentForm(forms.ModelForm):
     class Meta:
         model = Comment
         fields = ['content']
+        widgets = {
+            'content': forms.Textarea(attrs={'rows': 3, 'cols': 40}),
+        }
--- posts/models.py
+++ posts/models.py
@@ -15,11 +15,14 @@ class Post(models.Model):
     def __str__(self):
         return self.title
 
+    def get_display_comments(self):
+        return self.comments.all()[:3]
+
 
 # Create your models here.
 
 class Comment(models.Model):
-    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, related_name='comments', on_delete=models.CASCADE)
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
