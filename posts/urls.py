3f0749c8aceb636f568b6a30b8a05b115bbb5d5b
mitatskni <mitatskni@gmail.com>
2023-05-15 18:12:41 +0200

    Implemented like and unlike functionality.

--- posts/urls.py
+++ posts/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 from . import views
 
-app_name= 'posts'
+app_name = 'posts'
 urlpatterns = [
     path('', views.PostListView.as_view(), name='post_list'),
     path('create/', views.PostCreateView.as_view(), name='post_create'),
     path('<int:pk>/', views.PostDetailView.as_view(), name='post_detail'),
     path('<int:pk>/comment/', views.CommentCreateView.as_view(), name='comment_create'),
+    path('like/<int:post_id>/', views.LikePostView.as_view(), name='like_post'),
 ]
--- posts/views.py
+++ posts/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.db.models import Count
+from django.shortcuts import render, get_object_or_404, redirect
 from django.views.generic import ListView, DetailView, CreateView
+from django.views import View
 from django.contrib.auth.mixins import LoginRequiredMixin
 from .models import Post, Comment
 from .froms import PostFrom, CommentForm
@@ -11,6 +13,16 @@ class PostListView(LoginRequiredMixin, ListView):
     template_name = 'posts/post_list.html'
     context_object_name = 'post_list'
     paginate_by = 10
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['comment_form'] = CommentForm()
+        return context
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        queryset = queryset.annotate(like_count=Count('likes'))
+        return queryset
 
 
 class PostCreateView(LoginRequiredMixin, CreateView):
@@ -29,13 +41,35 @@ class PostDetailView(LoginRequiredMixin, DetailView):
     template_name = 'posts/post_detail.html'
     context_object_name = 'post'
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['comment_form'] = CommentForm()
+        return context
+
 
 class CommentCreateView(LoginRequiredMixin, CreateView):
     model = Comment
     template_name = 'posts/comment_create.html'
-    fields = ['content']
+    form_class = CommentForm
+    success_url = reverse_lazy('posts:post_list')
 
     def form_valid(self, form):
         form.instance.author = self.request.user
         form.instance.post = Post.objects.get(pk=self.kwargs['pk'])
         return super().form_valid(form)
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['comment_form'] = CommentForm()
+        return context
+
+class LikePostView(LoginRequiredMixin, View):
+    def post(self, request, *args, **kwargs):
+        post = get_object_or_404(Post, pk=self.kwargs['post_id'])
+        user = request.user
+        if user in post.likes.all():
+            post.likes.remove(user)
+        else:
+            post.likes.add(user)
+
+        return redirect('posts:post_list')
