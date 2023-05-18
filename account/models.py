afc283ae41e2aeae380f0483c76e00b6b84de7c7
mitatskni <mitatskni@gmail.com>
2023-05-18 21:08:12 +0200

    The "followers" field will be represented as a ManyToManyField, linking users who are following this user.

--- account/models.py
+++ account/models.py
@@ -6,6 +6,7 @@ class Profile(models.Model):
     user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     date_of_birth = models.DateField(blank=True, null=True)
     photo = models.ImageField(upload_to='users/%Y/%m/%d/', blank=True)
+    followers = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name='following')
 
     def __str__(self):
         return f'Profile of {self.user.username}'
