0cc63d9489bfe4944eb206797cba8c84d035029f
mitatskni <mitatskni@gmail.com>
2023-05-21 12:57:09 +0200

    I added `followers` and `following` fields to our model.

--- account/models.py
+++ account/models.py
@@ -3,10 +3,11 @@ from django.conf import settings
 
 
 class Profile(models.Model):
-    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='profile')
     date_of_birth = models.DateField(blank=True, null=True)
     photo = models.ImageField(upload_to='users/%Y/%m/%d/', blank=True)
-    followers = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name='following')
+    followers = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name='followers', blank=True)
+    following = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name='following', blank=True)
 
     def __str__(self):
         return f'Profile of {self.user.username}'
