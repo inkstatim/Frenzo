104934de3d9550f1a68b01c94fcac4020023ac9d
mitatskni <mitatskni@gmail.com>
2023-05-24 12:55:27 +0200

    I implemented the views for friend requests and user chat. I fixed the subscriptions, but the friend request is still being worked on.

--- account/urls.py
+++ account/urls.py
@@ -10,4 +10,10 @@ urlpatterns = [
     path('profile/<str:username>/', views.profile, name='profile'),
     path('follow/<str:username>/', views.follow, name='follow'),
     path('users/', views.user_list, name='user_list'),
+    path('send_friend_request/<int:to_user_id>/', views.send_friend_request, name='send_friend_request'),
+    path('accept_friend_request/<int:friendship_id>/', views.accept_friend_request, name='accept_friend_request'),
+    path('reject_friend_request/<int:friendship_id>/', views.reject_friend_request, name='reject_friend_request'),
+    path('friends/', views.friend_list, name='friend_list'),
+    path('message/create/<int:recipient_id>/', views.create_message, name='create_message'),
+    path('conversation/<int:recipient_id>/', views.conversation, name='conversation'),
 ]
--- account/views.py
+++ account/views.py
@@ -1,10 +1,11 @@
 from django.contrib.auth.models import User
+from django.db.models import Q
 from django.shortcuts import render, get_object_or_404, redirect
 from django.http import HttpResponse, HttpResponseRedirect
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.decorators import login_required
-from .forms import LoginForm, UserRegistrationForm, UserEditForm, ProfileEditForm
-from .models import Profile
+from .forms import LoginForm, UserRegistrationForm, UserEditForm, ProfileEditForm, MessageForm
+from .models import Profile, Friendship, Message
 from django.contrib import messages
 
 
@@ -71,7 +72,9 @@ def edit(request):
 @login_required
 def profile(request, username):
     user = get_object_or_404(User, username=username)
-    return render(request, 'account/profile.html', {'user': user})
+    recipient_id = user.id  # Используйте id пользователя в качестве recipient_id
+    return render(request, 'account/profile.html', {'user': user, 'recipient_id': recipient_id})
+
 
 @login_required
 def follow(request, username):
@@ -79,8 +82,7 @@ def follow(request, username):
 
     if request.method == 'POST':
         if request.user != user:
-            if request.user.profile.following.filter(profile__user=user).exists():
-
+            if request.user.profile.following.filter(username=username).exists():
                 request.user.profile.following.remove(user)
                 user.profile.followers.remove(request.user)
             else:
@@ -92,6 +94,78 @@ def follow(request, username):
     return HttpResponse("Invalid request")
 
 
+
 def user_list(request):
     users = User.objects.all()
     return render(request, 'account/user_list.html', {'users': users})
+
+
+@login_required
+def send_friend_request(request, to_user_id):
+    from_user = request.user
+    to_user = get_object_or_404(User, id=to_user_id)
+    friendship, created = Friendship.objects.get_or_create(from_user=from_user, to_user=to_user)
+    if created:
+        pass
+    # Если объект Friendship был только что создан, можно выполнить дополнительные действия, если необходимо
+
+    return redirect('profile', username=to_user.username)
+
+
+def accept_friend_request(request, friendship_id):
+    friendship = get_object_or_404(Friendship, id=friendship_id, to_user=request.user)
+    friendship.status = 'accepted'
+    friendship.save()
+    # Здесь можно выполнить дополнительные действия, связанные с принятием запроса на дружбу
+
+    return redirect('profile', username=friendship.from_user.username)
+
+
+@login_required
+def reject_friend_request(request, friendship_id):
+    friendship = get_object_or_404(Friendship, id=friendship_id, to_user=request.user)
+    friendship.status = 'rejected'
+    friendship.save()
+    # Здесь можно выполнить дополнительные действия, связанные с отклонением запроса на дружбу
+
+    return redirect('profile', username=friendship.from_user.username)
+
+
+@login_required
+def friend_list(request):
+    # Логика для получения списка друзей
+    friends = request.user.profile.following.all()
+    return render(request, 'account/friend_list.html', {'friends': friends})
+
+
+@login_required
+def create_message(request, recipient_id):
+    recipient = get_object_or_404(User, id=recipient_id)
+    form = MessageForm(request.POST or None)
+    if form.is_valid():
+        message = form.save(commit=False)
+        message.sender = request.user
+        message.recipient = recipient
+        message.save()
+        return redirect('conversation', recipient_id=recipient_id)
+    return render(request, 'account/create_message.html', {'form': form, 'recipient_id': recipient_id})
+
+
+@login_required
+def conversation(request, recipient_id):
+    recipient = get_object_or_404(User, id=recipient_id)
+    messages = Message.objects.filter(
+        Q(sender=request.user, recipient=recipient) | Q(sender=recipient, recipient=request.user)).order_by('timestamp')
+    form = MessageForm()
+
+    if request.method == 'POST':
+        form = MessageForm(request.POST)
+        if form.is_valid():
+            message = form.save(commit=False)
+            message.sender = request.user
+            message.recipient = recipient
+            message.save()
+            form = MessageForm()  # Очистка формы после успешной отправки
+
+    context = {'recipient': recipient, 'messages': messages, 'form': form}
+    return render(request, 'account/conversation.html', context)
